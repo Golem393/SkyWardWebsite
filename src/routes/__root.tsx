fac6522b4b91698e9626088bbb951628838896a1
jacob2011402 <jlim2011402@gmail.com>
2026-06-23 16:03:16 +0900

    change posthog API host

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -124,7 +124,7 @@ function RootShell({ children }: { children: ReactNode }) {
         <PostHogProvider
           apiKey={import.meta.env.VITE_PUBLIC_POSTHOG_PROJECT_TOKEN!}
           options={{
-            api_host: "/ingest",
+            api_host: "https://us.i.posthog.com",
             ui_host: import.meta.env.VITE_PUBLIC_POSTHOG_HOST || "https://us.posthog.com",
             defaults: "2025-05-24",
             capture_exceptions: true,
